6fced412275f75f8e147c8f9c866eb3712870506
Gad Hatem <gadh.24@gmail.com>
2020-10-18 16:58:02 +0200

    initial commit

---
+++ Scripts/feed_forward_neural_network.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Oct 18 16:48:00 2020
+
+@author: gad
+"""
+
+import pandas as pd
+
+mnist_df = pd.read_csv('../Data/mnist_train.csv', sep=',')
